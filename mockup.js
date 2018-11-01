1956020befe6eb470aa900b096497f0862ebdb25
SHollatz <ztallohs@gmail.com>
2018-11-01 08:54:45 -0700

    changed console.logs

--- mockup.js
+++ mockup.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-  console.log("inside ready")
+  console.log("document has loaded")
   $(document).on("click", ".btn", function(event) {
     event.preventDefault();
-    console.log($("#address-search").val());
-    console.log($("#activity_details_request_type").children("option:selected").text());
+    console.log("submit button was clicked")
+    console.log("Location: ", $("#address-search").val());
+    console.log($("Selected value from dropdown", "#activity_details_request_type").children("option:selected").text());
   })
 })
